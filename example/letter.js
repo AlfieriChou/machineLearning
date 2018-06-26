ffc01e814ebab83d997c21f0b2a26be5e7ce54c6
AlfieriChou <alfierichou@look.com>
2018-06-26 11:52:14 +0800

    example

---
+++ example/letter.js
@@ -0,0 +1,60 @@
+const brain = require('brain.js')
+
+const a = character(
+  '.#####.' +
+  '#.....#' +
+  '#.....#' +
+  '#######' +
+  '#.....#' +
+  '#.....#' +
+  '#.....#'
+)
+const b = character(
+  '######.' +
+  '#.....#' +
+  '#.....#' +
+  '######.' +
+  '#.....#' +
+  '#.....#' +
+  '######.'
+)
+const c = character(
+  '#######' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#######'
+)
+
+const net = new brain.NeuralNetwork()
+net.train([
+  { input: a, output: { a: 1 } },
+  { input: b, output: { b: 1 } },
+  { input: c, output: { c: 1 } }
+])
+
+const result = brain.likely(character(
+  '.#####.' +
+  '#.....#' +
+  '#.....#' +
+  '###.###' +
+  '#.....#' +
+  '#.....#' +
+  '#.....#'
+), net)
+
+console.log(result)
+
+function character (string) {
+  return string
+    .trim()
+    .split('')
+    .map(integer)
+}
+
+function integer (character) {
+  if (character === '#') return 1
+  return 0
+}
